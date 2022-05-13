a34d499a4fa768f0bf4dd5ee5e22ed61480ab3cc
babloo kumar <916bablookumar@gmail.com>
2022-05-14 00:38:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const targetdiv=document.getElementById('answer1');
+const btnm=document.getElementById('questins');
+btnm.onclick = function(){
+    if(targetdiv.style.display!=="none"){
+        targetdiv.style.display ="none";
+    }else{
+        targetdiv.style.display ="block";
+    }
+
+}
+const target=document.getElementById('answer2');
+const btn=document.getElementById('question1');
+btn.onclick = function(){
+    if(target.style.display!=="none"){
+        target.style.display ="none";
+    }else{
+        target.style.display ="block";
+    }
+
+}
+const tar=document.getElementById('answer3');
+const bt=document.getElementById('question2');
+bt.onclick = function(){
+    if(tar.style.display!=="none"){
+        tar.style.display ="none";
+    }else{
+        tar.style.display ="block";
+    }
+
+}
+
+
+const ta=document.getElementById('answer4');
+const b=document.getElementById('question3');
+b.onclick = function(){
+    if(ta.style.display!=="none"){
+        ta.style.display ="none";
+    }else{
+        ta.style.display ="block";
+    }
+
+}
+const t=document.getElementById('answer5');
+const bs=document.getElementById('question4');
+bs.onclick = function(){
+    if(t.style.display!=="none"){
+        t.style.display ="none";
+    }else{
+        t.style.display ="block";
+    }
+
+}
+const ti=document.getElementById('answer6');
+const bas=document.getElementById('question5');
+bas.onclick = function(){
+    if(ti.style.display!=="none"){
+        ti.style.display ="none";
+    }else{
+        ti.style.display ="block";
+    }
+
+}
